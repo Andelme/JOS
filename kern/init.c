53a3652233e868e95ccf3fba18d1ffbeff70f3a6
Vitaly Cheptsov <cheptsov@ispras.ru>
2019-08-26 21:39:01 +0300

    lab5: initial version

--- kern/init.c
+++ kern/init.c
@@ -5,6 +5,7 @@
 #include <inc/assert.h>
 
 #include <kern/monitor.h>
+#include <kern/tsc.h>
 #include <kern/console.h>
 #include <kern/env.h>
 #include <kern/trap.h>
@@ -27,6 +28,8 @@ i386_init(void)
 	// Can't call cprintf until after we do this!
 	cons_init();
 
+	tsc_calibrate();
+
 	cprintf("6828 decimal is %o octal!\n", 6828);
 	cprintf("END: %p\n", end);
 
@@ -44,6 +47,8 @@ i386_init(void)
 	ENV_CREATE_KERNEL_TYPE(prog_test2);
 	ENV_CREATE_KERNEL_TYPE(prog_test3);
 	ENV_CREATE_KERNEL_TYPE(prog_test4);
+	ENV_CREATE_KERNEL_TYPE(prog_test5);
+	ENV_CREATE_KERNEL_TYPE(prog_test6);
 #endif
 
 	// Schedule and run the first user environment!
